e93212d56214a5894bd642284b71b2f1b7a50679
Michelle <mchristine.stone91@gmail.com>
2022-01-06 14:27:37 -0500

    added apiRoutes for post and get

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,3 +1,31 @@
 const router = require("express").Router();
+const fs = require("fs");
+const path = require("path");
+const { v4: uuidv4 } = require("uuid");
+
+router.get("/notes", (req, res) => {
+  fs.readFile("db/db.json", "utf-8", (err, data) => {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  });
+});
+
+router.post("/notes", (req, res) => {
+  var currentNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4(),
+  };
+  fs.readFile("db/db.json", "utf-8", (err, data) => {
+    var currentDb = JSON.parse(data);
+    currentDb.push(currentNote);
+    fs.writeFile("db/db.json", JSON.stringify(currentDb), (err) => {
+      err ? console.log(err) : console.log("New Note Saved");
+    });
+    res.sendFile(path.join(__dirname, "../../public/notes.html"));
+  });
+});
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -2,12 +2,16 @@ const express = require("express");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require("./routes/apiRoutes");
+const apiRoutes = require("./routes/apiRoutes/noteRoutes");
 const htmlRoutes = require("./routes/htmlRoutes");
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
 
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port http://localhost:${PORT}`);
 });
